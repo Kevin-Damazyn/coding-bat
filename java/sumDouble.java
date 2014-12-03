4064859c40abb5958929eecbc461b4802c080429
Kevin-Damazyn <damazynk@slu.edu>
2014-12-03 12:36:45 -0600

    sumDouble coding bat

---
+++ java/sumDouble.java
@@ -0,0 +1,28 @@
+/*
+Kevin Damazyn 
+	
+Given two int values, return their sum. 
+Unless the two values are the same, then 
+return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*
+*/
+
+class sumDouble {
+	
+	public static void main(String[] args) {
+		System.out.println(sumDouble(1, 2));
+		System.out.println(sumDouble(3, 2));
+		System.out.println(sumDouble(2, 2));
+	}
+
+	public static int sumDouble(int a, int b) {
+		if (a == b) { return 2*(a+b);}
+		return a+b;
+	}
+
+
+}
